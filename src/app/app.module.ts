f2477d7634afde44b359d772a818c1bda5345322
slk <slk@WSLK1GFTR3C-TR.SLKSOFT.COM>
2018-01-21 15:03:23 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CreateAuditComponent } from './components/create-audit/create-audit.component';
+import { AuditListComponent } from './components/audit-list/audit-list.component';
+import { AuditDetailsComponent } from './components/audit-details/audit-details.component';
+import { EditAuditComponent } from './components/edit-audit/edit-audit.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CreateAuditComponent,
+    AuditListComponent,
+    AuditDetailsComponent,
+    EditAuditComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/components/audit-list/audit-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-audit-list',
+  templateUrl: './audit-list.component.html',
+  styleUrls: ['./audit-list.component.css']
+})
+export class AuditListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
